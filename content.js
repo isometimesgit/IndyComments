afa64801defcee40cf9c667d90ded2709f857a86
Ed Williams <ed.williams.mail@gmail.com>
2020-12-19 14:51:14 +0000

    updated icons

--- content.js
+++ content.js
@@ -1,15 +1,8 @@
-chrome.storage.local.get(defaultSettings, (settings) => {
-    setInterval(() => {
-        walkComments(settings)
-    }, 2000)
-})
-function walkComments(settings) {
-    document.querySelectorAll('span.comment-username').forEach((username) => {
-        formatComment(username.parentElement.parentElement)
+setTimeout(walkComments, 1000)
+setInterval(walkComments, 5000)
+function walkComments() {
+    document.querySelectorAll('.comment-list-wrapper .comment-body').forEach((body) => {
+        body.innerHTML = body.innerHTML.replaceAll('<br>', '\n')
+        body.innerHTML = body.textContent;
     })
-}
-function formatComment(comment) {
-    let body = comment.querySelector('.comment-body');
-    body.innerHTML = body.innerHTML.replace(/<br\s*[\/]?>/gi, '\n')
-    body.innerHTML = body.textContent;
 }
